dd0bba353826d03f677e972fb1322a8afcc5c03a
GdulskiJ <s27753@pjwstk.edu.pl>
2026-01-15 04:49:39 +0100

    aaa

---
+++ b.py
@@ -0,0 +1,19 @@
+from datetime import date
+import meteostat as ms
+
+# Set time period
+start = date(2010, 1, 1)
+end = date(2026, 1, 15)
+
+# Get daily data
+ts = ms.daily(ms.Station(id='12375'), start, end)
+df = ts.fetch()
+print(df)
+
+df.to_csv("warszawa_history_full.csv")
+print(f"\n✅ Dane dla Warszawy zapisane: warszawa_history_full.csv")
+print(f"Łącznie rekordów: {len(df)}")
+# Print Da
+# point = ms.Point(52.23, 21.01, 100)  # Example: Frankfurt, Germany
+# stations = ms.stations.nearby(point, limit=4)
+# print(stations)
